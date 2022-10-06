06260b2599f801339f32da004dc2d30d0d8ff4c0
David Moore <david@robokiwi.com>
2022-10-07 12:02:57 +1300

    Word count

---
+++ tests/SiteGen.Tests.UnitTests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,14 @@
+﻿using SiteGen.Core.Extensions;
+
+namespace SiteGen.Tests.UnitTests.Extensions
+{
+    public class StringExtensionsTests
+    {
+        [Theory]
+        [InlineData("  This has four words. ", 4)]
+        public void WordCount(string value, int expected)
+        {
+            Assert.Equal(value.AsSpan().WordCount(), expected);
+        }
+    }
+}
---
+++ tests/SiteGen.Tests.UnitTests/Processors/WordCountProcessorTests.cs
@@ -0,0 +1,22 @@
+﻿using SiteGen.Core.Models;
+using SiteGen.Core.Services;
+
+namespace SiteGen.Tests.UnitTests.Processors
+{
+    public class WordCountProcessorTests
+    {
+        [Fact]
+        public async Task ProcessAsync()
+        {
+            var processor = new WordCountProcessor();
+
+            var node = new SiteNode {
+                ContentPlainText = " My content has five words. "
+            };
+
+            await processor.ProcessAsync(node);
+
+            Assert.Equal(5, node.WordCount);
+        }
+    }
+}
