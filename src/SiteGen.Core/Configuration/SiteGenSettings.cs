d98fce42afc8466c4b85687dcc5a6618360bf461
David Moore <david@robokiwi.com>
2022-10-08 20:55:46 +1300

    Default settings

---
+++ src/SiteGen.Core/Configuration/SiteGenSettings.cs
@@ -0,0 +1,20 @@
+﻿namespace SiteGen.Core.Configuration
+{
+    public class SiteGenSettings
+    {
+        public SiteGenSettings() {
+
+            Taxonomies = new Dictionary<string, string?>()
+            {
+                {"category", "categories" },
+                {"tag", "tags" }
+            };
+
+            ContentPaths = new List<string> { "content" };
+        }
+
+        public IDictionary<string, string?> Taxonomies { get; set; }
+
+        public IList<string> ContentPaths { get; set; }
+    }
+}
