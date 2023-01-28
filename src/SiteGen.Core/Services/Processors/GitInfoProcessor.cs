673f482c07200902015ddd626ca23a32a6cad106
David Moore <david@robokiwi.com>
2023-01-29 01:33:31 +1300

    Deprecate libgit2sharp, and invoke git directly to get commit information

--- src/SiteGen.Core/Services/Processors/GitInfoProcessor.cs
+++ src/SiteGen.Core/Services/Processors/GitInfoProcessor.cs
@@ -1,9 +1,5 @@
-﻿using LibGit2Sharp;
-using SiteGen.Core.Configuration;
-using SiteGen.Core.Configuration.Yaml;
-using SiteGen.Core.Models;
-using System.Text;
-using Tommy.Extensions.Configuration;
+﻿using SiteGen.Core.Models;
+using System.Diagnostics;
 
 namespace SiteGen.Core.Services.Processors;
 
@@ -11,58 +7,34 @@ namespace SiteGen.Core.Services.Processors;
 /// Updates the node front matter and metadata with
 /// information from its last Git commit.
 /// </summary>
-public class GitInfoProcessor : ISiteNodeProcessor, IInitializable
+public class GitInfoProcessor : ISiteNodeProcessor
 {
-    private readonly SiteGenSettings settings;
-
-    public GitInfoProcessor(SiteGenSettings settings)
-    {
-        this.settings = settings;
-    }
-
-    public async Task InitializeAsync()
-    {
-        //var path = Path.GetFullPath(settings.ContentPaths.First());
-        //var repoPath = Repository.Discover(path);
-        //if (repoPath == null) return;
-
-        //using (var repo = new Repository(repoPath))
-        //{
-        //    var relativePath = Path.GetRelativePath(repoPath, path);
-        //    var normalizedPath = relativePath.TrimStart('.').TrimStart('\\').Replace('\\', '/');
-
-        //    var lookupList = new[] {path};
-        //    var explicitPathOptions = new ExplicitPathsOptions { };
-        //    var compareOptions = new CompareOptions() { IncludeUnmodified = true, Similarity = SimilarityOptions.None };
-        //    var files = repo.Diff.Compare<TreeChanges>((Tree)null, DiffTargets.Index, lookupList, explicitPathOptions, compareOptions);
-        //}
-    }
-
     public async Task ProcessAsync(SiteNode node)
     {
-        var repoPath = Repository.Discover(node.Path);
-        if (repoPath == null) return;
-        
-        var filter = new CommitFilter()
+        var startInfo = new ProcessStartInfo
         {
-            SortBy = CommitSortStrategies.Time | CommitSortStrategies.Reverse
+            FileName = "git",
+            Arguments = $"log -1 --pretty=format:\"%aI '%an' '%ae'\" \"{node.Path}\"",
+            RedirectStandardOutput = true,
+            RedirectStandardError = true
         };
 
-        IDictionary<string,Commit> commits = new Dictionary<string,Commit>();
+        var process = Process.Start(startInfo)!;
+        process.WaitForExit(1000);
 
-        using (var repo = new Repository(repoPath))
+        var output = process.StandardOutput.ReadToEnd();
+        var error = process.StandardError.ReadToEnd();
+
+        var chunks = output.Trim().Split(' ');
+
+        if(!string.IsNullOrWhiteSpace(error) || chunks.Length == 0 || string.IsNullOrWhiteSpace(chunks[0]))
         {
-            var relativePath = Path.GetRelativePath(repoPath, node.Path);
-            var normalizedPath = relativePath.TrimStart('.').TrimStart('\\').Replace('\\','/');
-
-            var log = repo.Commits.QueryBy(normalizedPath).FirstOrDefault();
-
-            if( log == null) return;
-
-            var commit = log.Commit;
-
-            node.Date = commit.Author.When;
-            node.DateModified = commit.Author.When;
+            return;
         }
+
+        var authorDate = DateTimeOffset.Parse(chunks[0]);
+
+        node.Date ??= authorDate;
+        node.DateModified = authorDate;
     }
 }
