473d7293ceeaa406cec9204ef98e2d1a28c62942
David Moore <david@robokiwi.com>
2025-06-07 18:41:13 +1200

    Put test back to PageTest base

--- tests/SiteGen.Tests/Extensions/Markdown/MonacoTests.cs
+++ tests/SiteGen.Tests/Extensions/Markdown/MonacoTests.cs
@@ -1,31 +1,17 @@
-using Microsoft.Playwright;
 using Microsoft.Playwright.MSTest;
 using SiteGen.Extensions.Markdown.Monaco;
 
 namespace SiteGen.Tests.Extensions.Markdown;
 
 [TestClass]
-public class MonacoTests //: PageTest
+public class MonacoTests : PageTest
 {
-    public TestContext TestContext { get; set; } = null!;
-
     [TestMethod]
     public async Task MonacoHighlighter()
     {
-        using var playwright = await Playwright.CreateAsync();
-        var options = new BrowserTypeLaunchOptions
-        {
-            Headless = true,
-            Devtools = true,
-            Channel = "msedge"
-        };
-        await using var browser = await playwright.Chromium.LaunchAsync(options);
-        var contextOptions = new BrowserNewContextOptions { };
-        var context = await browser.NewContextAsync(contextOptions);
-        var page = await context.NewPageAsync();
-
         var directory = new DirectoryInfo(Path.Combine(TestContext.DeploymentDirectory, ".monaco"));
-        await using var host = new MonacoHost(page, directory);
+        await using var host = new MonacoHost(Page, directory);
+        
         var result = await host.Highlight("const test=\"value\";", "javascript");
 
         var expected = "<span><span class=\"mtk6\">const</span><span class=\"mtk1\"> test=</span><span class=\"mtk20\">\"value\"</span><span class=\"mtk1\">;</span></span><br/>";
